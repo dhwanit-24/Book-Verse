a118ee24dcd33d8deae9bae89e03893c39104e50
dhwanit-24 <dhwanit2426@gmail.com>
2026-04-08 21:27:09 +0530

    feat: add dynamic serial numbers and centered update button in manage users page

--- src/main/java/com/example/demo/repository/UserRepo.java
+++ src/main/java/com/example/demo/repository/UserRepo.java
@@ -1,5 +1,7 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.UserEntity;
@@ -10,5 +12,5 @@ public interface UserRepo extends JpaRepository<UserEntity, Integer>{
 	
 	UserEntity findByEmail(String email);
 
-	
+	List<UserEntity> findAllByOrderByIdDesc();
 }
